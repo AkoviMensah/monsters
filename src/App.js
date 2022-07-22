30ae37b23df62dfb184e2bac05cb6f33561a0a28
Akovi Mensah <akovi66@gmail.com>
2022-07-21 21:22:22 -0400

    new styling

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
       searchField.toLocaleLowerCase()
     ))
     return <div className='App'>
+      <h1> Monsters </h1>
       <SearchBox placeholder='search monsters'
       handleChange={this.handleChange}/>
       <CardList monsters={filteredMonsters}/>
