9ddcac5cbbe576f7ae807603082c4f0b7d6333a8
Akovi Mensah <akovi66@gmail.com>
2022-08-09 15:35:48 -0500

    updated src images

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,7 @@
 import "./card.css"
 export const Card = (props) => {
     return <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}/>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set5&size=180x180`} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
